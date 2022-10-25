5a407bdebafb8d36f99b487231f9e44ffb1447e1
Anita <aneeta.ashraf7@gmail.com>
2022-10-25 23:30:17 +0300

    Define JS function bingo

--- coding exercises.js
+++ coding exercises.js
@@ -1,5 +1,15 @@
+// Excersize number 5
 function string(text) {
     return (text);
   }
   let strText = string('How are you?')
   console.log(strText);
+
+ // Excersize number 6
+
+function number(a){
+    if (a > 5)
+    return ("Bingo");
+  }
+  console.log(number(7)); // will print Bingo
+  console.log(number(0)); // will print undefined
