a8030ad9f5ae8bd90132e5de83ff15b7487ce8ab
Anita <aneeta.ashraf7@gmail.com>
2022-11-01 23:14:12 +0200

    Revised version of EX 5 and 6

---
+++ codingexercise.js
@@ -0,0 +1,20 @@
+//Example of Excersize 5 
+
+  const myText = function ()  {
+    return "Finland, is , beautiful";
+
+  };
+  console.log(myText()); // will print Finland, is, beautiful 
+
+   // Excersize number 6
+function number(a){
+    if (a > 5) {
+    return ("Bingo");
+  } else {
+      return ("Cat");
+  }
+};
+
+  console.log(number(7)); // will print Bingo
+  console.log(number(0)); // will print undefined
+  console.log(number(11)); // will print Bingo
